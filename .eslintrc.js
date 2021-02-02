c122610ec2d4a1ec917df21783b54203521a0cff
evd3v <evd3v@WIN-C9CG7DNGEFI>
2021-02-02 14:24:24 +0300

    init configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,19 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  extends: [
+    'plugin:vue/recommended',
+    'eslint:recommended',
+    'prettier/vue',
+    'plugin:prettier/recommended',
+  ],
   parserOptions: {
     parser: "babel-eslint"
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    'no-unused-vars': process.env.NODE_ENV === "production" ? "warn" : "off",
+    'vue/no-unused-components': process.env.NODE_ENV === "production" ? "warn" : "off",
   }
 };
